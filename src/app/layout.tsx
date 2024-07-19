60921e9201fb923108fd076fc58ecfe6d1a11df3
Daniel <danielribas1105@gmail.com>
2024-07-19 10:57:47 -0300

    create new nextjs study project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,8 +15,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="pt-BR">
+      <body className="bg-zinc-950 text-white">{children}</body>
     </html>
   );
 }
